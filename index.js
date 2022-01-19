f715c7acb3239f575ebd834f41abaeb411f46069
Brendan <brendankalis2@gmail.com>
2022-01-19 03:24:22 -0600

    commit tree dc01a894ce9f929cfaeae2928cd4abff2bf4e860

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(symbol = "*") {
+    
+    const encouragement = function (positive = "special") {
+        return `You are ${symbol}${positive}${symbol}!`
+    }
+    return encouragement
+}
+wrapAdjective("||")("a dedicated programmer")
